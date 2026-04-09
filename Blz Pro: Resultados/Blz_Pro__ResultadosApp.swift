faa747dd8912d6f79a20e170898cf830d2b2d432
Tymur Batulin <batulintimur@gmail.com>
2026-04-09 22:47:48 +0200

    Initial Commit

---
+++ Blz Pro: Resultados/Blz_Pro__ResultadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blz_Pro__ResultadosApp.swift
+//  Blz Pro: Resultados
+//
+//  Created by Tymur Batulin on 09.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Blz_Pro__ResultadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
